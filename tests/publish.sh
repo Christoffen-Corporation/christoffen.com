99936f54fe05c1b2907d87f72a59b6eeb45a4aea
M. Gage Morgan <gagemorgan@outlook.com>
2016-11-09 10:22:00 -0500

    Update and rename travis.sh to publish.sh

---
+++ tests/publish.sh
@@ -0,0 +1,7 @@
+# build site
+sass /sass/main.sass styles.css;
+mkdir output;
+
+git config --global user.email 'gagemorgan@outlook.com';
+git config --global user.name 'mgagemorgan';
+
--- tests/travis.sh
+++
@@ -1,5 +0,0 @@
-apt-get install ruby;
-gem install slim sass;
-sass ../sass/main.sass triggered.css;
-slimrb ../bombsite.slim triggered.html;
-
